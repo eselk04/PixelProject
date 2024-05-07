e07cd44a75c1222dcd6ab3d3bb7297d7822f8dbc
berh0 <berham10@gmail.com>
2024-05-07 16:09:54 +0300

    admin panel product list

---
+++ Pixel_Web_Project-main/PixelAdmin/login.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="login.css"/>
+    
+    <title>Login</title>
+</head>
+<body>
+ <?php
+ session_start();
+ if (isset($_POST["admin_login"])) {
+    if ($_POST["password"] == "admin") {
+        $_SESSION["admin"] = true;
+        header("location:pixeladmin.php");
+    }
+    else {
+        $_SESSION["admin"] = false;
+        echo"Hatalı Şifre";
+    }
+ }
+
+ ?>
+
+
+    <section>
+        <div class="form" >
+            <h1>Admin Panel</h1>
+            <form method = "POST">
+                <div class="form-control">
+                    <input type="text" id="email" name="email" required>
+                    <label >Name</label>
+                </div>
+                <div class="form-control">
+                    <input type="password" id="password" name="password" required>
+                    <label >Password</label>
+                </div>
+
+                <button id="admin_login" name="admin_login" class="formbtn">Login</button>
+            </form>
+        </div>
+    </section>
+    <?php 
+
+
+
+ 
+
+    
+    
+    
+    
+    ?>
+
+    <script src="login.js"></script>
+</body>
+</html>
--- Pixel_Web_Project-main/PixelAdmin/pixeladmin.php
+++ Pixel_Web_Project-main/PixelAdmin/pixeladmin.php
@@ -1,3 +1,84 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="pixeladmin.css">
+    <title>Pixel Shop</title>
+</head>
+<body>
 
-?>
+<div class="navbar">
+    <a href="#">Home</a>
+    <a href="#">Order Management</a>
+    <input type="text" placeholder="Search orders...">
+</div>
+
+<div class="content">
+    <h2>Product List</h2>
+    <div class="product-container">
+        <?php
+        require "../common/dbconnect.php";
+       
+        $query =  "SELECT * FROM products p where p.productname ilike " . " '%" . "%'" ;
+        $result = pg_query($dbconn, $query);
+
+    if (!$result) {
+        echo "Sorgu çalıştırma hatası: " . pg_last_error();
+        exit;
+    }
+
+
+
+    echo "<div class='product'>";
+        echo "<span>ID</span>";
+        echo "<span>Name</span>";
+        echo "<span>Price</span>";
+        echo "<span>Options</span>";
+        echo "</div>";
+    while ($row = pg_fetch_assoc($result))
+    {
+        if (isset($_POST["delete{$row["productid"]}"])) {
+            $querycart = "DELETE FROM carts WHERE product_id=". $row["productid"];
+            pg_query($dbconn, $querycart);
+       
+                $querydelete = "DELETE FROM products WHERE productid=". $row["productid"];
+                pg_query($dbconn, $querydelete);
+                header("location:pixeladmin.php");
+        } 
+        echo "<div class='product'>";
+        echo "<span>{$row["productid"]} </span>";
+        echo "<span>{$row["productname"]} </span>";
+        echo "<span>{$row["price"]} </span>";
+        
+        echo "<form method= \"post\">";
+        echo "<a href=\"edit_product.php?id={$row["productid"]}\">Edit</a> ";
+        echo "<button id='delete{$row["productid"]}' name='delete{$row["productid"]}' type=\'submit'>Delete</button>";
+        echo "</form>";
+        echo "</div>";
+    }          
+        ?>
+
+    </div>
+    <h2>Order Management</h2>
+    <div class="order-container">
+        <?php
+        // Assuming $orders is an array containing order data
+            echo "<class='order'>";
+            echo "<span>ID: </span>";
+            echo "<span>Date: </span>";
+            echo "<span>Customer:</span>";
+            echo "<a href=\"edit_order.php?id=\">Edit</a> ";
+            echo "<form method= \"post\">";
+            echo "<a href=\"delete_order.php?id=\">Delete</a>";
+            echo "</form>";
+         
+            echo "</div>";
+        ?>
+    </div>
+</div>
+
+</body>
+</html>
